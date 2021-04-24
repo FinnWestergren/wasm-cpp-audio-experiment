2dbcd412ab304f84659f905ade875579ae641381
Finn Westergren <fwesterg5@gmail.com>
2021-04-24 12:03:32 -0400

    wip

--- cpp/index.cpp
+++ cpp/index.cpp
@@ -4,9 +4,33 @@
 extern "C" {
 #endif
 
+static int c = 0;
+
+EMSCRIPTEN_KEEPALIVE
+void setC(int a, int b) {
+}
+
 EMSCRIPTEN_KEEPALIVE 
-int myCustomAddMethod(int a, int b) {
-    return a + b;
+int getC() {
+    return c;
+}
+
+// unsigned query_sample_rate_of_audio {
+//     return EM_ASM_INT({
+//         var AudioContext = window.AudioContext || window.webkitAudioContext;
+//         var ctx = new AudioContext();
+//         var sr = ctx.sampleRate;
+//         ctx.close();
+//         return sr;
+//     });
+// }
+
+void run() {
+    c += 1;
+}
+
+int main(){
+    emscripten_set_main_loop(run, 1, 1);
 }
 
 #ifdef __cplusplus
