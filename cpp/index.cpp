7f705829ea4131a8ece1b96730fbdfd85b7b74d2
FinnWestergren <fwesterg@skidmore.edu>
2021-04-25 12:53:29 -0400

    wip wip

--- cpp/index.cpp
+++ cpp/index.cpp
@@ -4,33 +4,28 @@
 extern "C" {
 #endif
 
-static int c = 0;
+static unsigned SYSTEM_SAMPLE_RATE = 44100;
+static const unsigned BUFFER_SIZE = 200;
+static const unsigned BIT_DEPTH = 16;
 
-EMSCRIPTEN_KEEPALIVE
-void setC(int a, int b) {
+unsigned query_sample_rate_of_audiocontexts() {
+    return EM_ASM_INT({
+        var AudioContext = window.AudioContext || window.webkitAudioContext;
+        var ctx = new AudioContext();
+        var sr = ctx.sampleRate;
+        ctx.close();
+        return sr;
+    });
 }
 
-EMSCRIPTEN_KEEPALIVE 
-int getC() {
-    return c;
+void audioLoop() {
+    
 }
 
-// unsigned query_sample_rate_of_audio {
-//     return EM_ASM_INT({
-//         var AudioContext = window.AudioContext || window.webkitAudioContext;
-//         var ctx = new AudioContext();
-//         var sr = ctx.sampleRate;
-//         ctx.close();
-//         return sr;
-//     });
-// }
-
-void run() {
-    c += 1;
-}
-
-int main(){
-    emscripten_set_main_loop(run, 1, 1);
+int main() {
+    SYSTEM_SAMPLE_RATE = query_sample_rate_of_audiocontexts();
+    unsigned samplesPerBuffer = SYSTEM_SAMPLE_RATE / BUFFER_SIZE;
+    emscripten_set_main_loop(audioLoop, samplesPerBuffer, 1);
 }
 
 #ifdef __cplusplus
