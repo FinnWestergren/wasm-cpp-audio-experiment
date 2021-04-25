e7fbed147b49cd57c1ee68797dd3a7d22ab55ff6
Finn Westergren <fwesterg5@gmail.com>
2021-04-25 15:23:39 -0400

    import proper libraries

---
+++ build-and-deploy.cmd
@@ -0,0 +1,1 @@
+.\generate-wasm.cmd && npm run deploy
--- cpp/index.cpp
+++ cpp/index.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <iostream>
 #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
+#include <AL/al.h>
+#include <AL/alc.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -7,6 +12,8 @@ extern "C" {
 static unsigned SYSTEM_SAMPLE_RATE = 44100;
 static const unsigned BUFFER_SIZE = 200;
 static const unsigned BIT_DEPTH = 16;
+static void* test = NULL;
+
 
 unsigned query_sample_rate_of_audiocontexts() {
     return EM_ASM_INT({
@@ -24,8 +31,11 @@ void audioLoop() {
 
 int main() {
     SYSTEM_SAMPLE_RATE = query_sample_rate_of_audiocontexts();
-    unsigned samplesPerBuffer = SYSTEM_SAMPLE_RATE / BUFFER_SIZE;
-    emscripten_set_main_loop(audioLoop, samplesPerBuffer, 1);
+    test = alcOpenDevice(NULL);
+    unsigned buffersPerSecond = SYSTEM_SAMPLE_RATE / BUFFER_SIZE;
+    std::cout << "buffers per second: " << buffersPerSecond << std::endl;
+    emscripten_set_main_loop(audioLoop, buffersPerSecond, 1);
+    return 0;
 }
 
 #ifdef __cplusplus
